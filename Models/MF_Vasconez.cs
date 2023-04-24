3eb5b8be761e5f161dd58a1ab4f6a369b424228b
laboratorio <laboratorio@UPOAULA10204>
2023-04-24 07:37:23 -0500

    Agregar archivos de proyecto.

---
+++ Models/MF_Vasconez.cs
@@ -0,0 +1,12 @@
+﻿namespace MV_Examen.Models
+{
+    public class MF_Vasconez
+    {
+        public int mv_edad { get; set; }
+        public decimal mv_alario { get; set; }
+        public string? mv_descricao { get; set; }
+        public bool mv_laborando { get; set; }
+        public DateTime? fecha  { get; set; }
+
+    }
+}
