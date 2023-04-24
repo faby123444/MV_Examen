b1a8c44e346fac61ef71a3ada92f981155d2240c
Faby121753 <m.f.vasconez@hotmail.com>
2023-04-24 16:50:34 -0500

    Listo

--- Models/MF_Vasconez.cs
+++ Models/MF_Vasconez.cs
@@ -1,9 +1,17 @@
-﻿namespace MV_Examen.Models
+﻿using MessagePack;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace MV_Examen.Models
 {
     public class MF_Vasconez
     {
+
+        [System.ComponentModel.DataAnnotations.Required]
         public int Id { get; set; }
+        [Range(450, 1000000)]
         public decimal Salariomf { get; set; }
+        [StringLength(10)]
         public string? Nombremf { get; set; }
         public bool Activomf { get; set; }
         public DateTime Cumpleañosmf { get; set; }
